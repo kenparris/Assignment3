87a150a7e701f7e0d38551db086df581b0229245
Ken <ken@MelkBook-5.local>
2026-02-17 14:00:48 -0500

    add project

---
+++ Assets/Scripts/Audio/Reactive.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class Reactive : MonoBehaviour
+{
+    // 2D arrays: [segmentIndex, sphereIndex]
+    // Row = time segment (0-10, 10-20, 20-30)
+    // Col = sphere id
+    // Example: startPositions[1, 25] -> sphere 25 start position in 10-20s segment
+
+    Vector3[,] startPositions;
+
+    Vector3[,] endPositions;
+    float[] timeFlags;
+
+    void Start()
+
+    {
+
+        timeFlags = new float[] { 0f, 10f, 20f, 30f };
+
+        segmentCount = timeFlags.Length - 1;
+
+        spheres = new GameObject[numSphere];
+
+        startPositions = new Vector3[segmentCount, numSphere];
+
+        endPositions = new Vector3[segmentCount, numSphere];
+
+
+        for (int i = 0; i < numSphere; i++)
+
+        {
+
+            // Segment 0 : 0s -> 11s
+
+            startPositions[0, i] = xxx;
+
+            endPositions[0, i] = xxx;
+
+
+            // Segment 1 & 3: 11s -> 50s     1:15s -> 1:54s
+
+            startPositions[1, i] = xxx;
+
+            endPositions[1, i] = xxx;
+
+
+            // Segment 2 & 4: 50s -> 1:15s    1:54s -> 2:18s
+
+            startPositions[2, i] = xxx;
+
+            endPositions[2, i] = xxx;
+
+            // Segment 5 : 2:18s -> 2:41s
+
+            startPositions[2, i] = xxx;
+
+            endPositions[2, i] = xxx;
+
+            // Segment 6 : 2:41s -> 2:55s
+
+            startPositions[2, i] = xxx;
+
+            endPositions[2, i] = xxx;
+
+}
+
+
+}
+
+    
+	  void Update()
+
+    {
+        float currentTime = Time.time;
+
+        if (currentTime < timeFlags[1])
+
+        {
+
+				
+
+        }
+
+        else if (currentTime < timeFlags[2])
+
+        {
+
+        }
+
+        else if (currentTime < timeFlags[3])
+
+        {
+
+        }
+
+        else
+
+        {
+
+ }
+
+  }
+}
