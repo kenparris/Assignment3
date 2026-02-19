454ac1a4cb4e164762eb7a5ba5feca6355e60e7c
Ken <ken@MelkBook-5.local>
2026-02-19 12:23:05 -0500

    update project

--- Assets/Scripts/Audio/AudioSpectrum.cs
+++ Assets/Scripts/Audio/AudioSpectrum.cs
@@ -3,6 +3,7 @@
 // Author: Myungin Lee
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent (typeof(AudioSource))]
@@ -10,9 +11,10 @@ using UnityEngine;
 public class AudioSpectrum : MonoBehaviour
 {
     AudioSource source;
-    public static int FFTSIZE = 1024; // https://en.wikipedia.org/wiki/Fast_Fourier_transform
+    public static int FFTSIZE = 4096; // https://en.wikipedia.org/wiki/Fast_Fourier_transform
     public static float[] samples = new float[FFTSIZE];
     public static float audioAmp = 0f;
+    float percussion, brass;
     void Start()
     {
         source = GetComponent<AudioSource>();       
@@ -26,6 +28,23 @@ public class AudioSpectrum : MonoBehaviour
         for (int i = 0; i < FFTSIZE; i++)
         {
             audioAmp += samples[i];
-        }        
+        }
+        percussion = 0f;
+        brass = 0f;
+
+        // 2~10 : percussion
+        for (int i = 2; i < 10; i++)
+        {
+            percussion += samples[i];
+        }
+
+        percussion = percussion / 8f;
+
+        // 25~50 : brass
+        for (int i = 25; i < 50; i++)
+        {
+            brass += samples[i];
+        }
+        brass = brass / 25f;
     }
 }
--- Assets/Scripts/Audio/Reactive.cs
+++ Assets/Scripts/Audio/Reactive.cs
@@ -7,16 +7,20 @@ public class Reactive : MonoBehaviour
     // Col = sphere id
     // Example: startPositions[1, 25] -> sphere 25 start position in 10-20s segment
 
+    GameObject[] spheres;
+    static int numSphere = 200;
+    int segmentCount;
     Vector3[,] startPositions;
 
     Vector3[,] endPositions;
     float[] timeFlags;
+    
 
     void Start()
 
     {
 
-        timeFlags = new float[] { 0f, 10f, 20f, 30f };
+        timeFlags = new float[] { 0f, 11f, 50f, 75f, 114f, 138f, 161f };
 
         segmentCount = timeFlags.Length - 1;
 
@@ -33,35 +37,35 @@ public class Reactive : MonoBehaviour
 
             // Segment 0 : 0s -> 11s
 
-            startPositions[0, i] = xxx;
+            startPositions[0, i] = Vector3.one;
 
-            endPositions[0, i] = xxx;
+            endPositions[0, i] = Vector3.one;
 
 
             // Segment 1 & 3: 11s -> 50s     1:15s -> 1:54s
 
-            startPositions[1, i] = xxx;
+            startPositions[1, i] = Vector3.one;
 
-            endPositions[1, i] = xxx;
+            endPositions[1, i] = Vector3.one;
 
 
             // Segment 2 & 4: 50s -> 1:15s    1:54s -> 2:18s
 
-            startPositions[2, i] = xxx;
+            startPositions[2, i] = Vector3.one;
 
-            endPositions[2, i] = xxx;
+            endPositions[2, i] = Vector3.one;
 
             // Segment 5 : 2:18s -> 2:41s
 
-            startPositions[2, i] = xxx;
+            startPositions[3, i] = Vector3.one;
 
-            endPositions[2, i] = xxx;
+            endPositions[3, i] = Vector3.one;
 
             // Segment 6 : 2:41s -> 2:55s
 
-            startPositions[2, i] = xxx;
+            startPositions[4, i] = Vector3.one;
 
-            endPositions[2, i] = xxx;
+            endPositions[4, i] = Vector3.one;
 
 }
 
